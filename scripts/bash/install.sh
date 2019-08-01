5f63531c21c78acfe75f52ea76129ef81885cd2a
Daniel Noyola <daniel.noyola.dev@gmail.com>
2019-07-31 21:53:36 -0600

    Install command.

---
+++ scripts/bash/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Database for the drush command.
+DB_URL="mysql://${DATABASE_USER:-drupal8}:${DATABASE_PASSWORD:-drupal8}@${DATABASE_HOST:-database}/${DATABASE_NAME:-drupal8}"
+
+if [ -e "${CONFIG_PATH}/default/system.site.yml" ]; then
+  # If config exists, install using it.
+  echo "Installing Drupal from the existing configuration."
+  drush si --db-url="${DB_URL}" --account-pass="admin" "${PROFILE}" --existing-config -y
+else
+  # Otherwise install clean from profile.
+  echo "Installing Drupal."
+  drush si --db-url="${DB_URL}" --account-pass="admin" "${PROFILE}" -y
+fi
