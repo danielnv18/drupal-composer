5da49c0211bf89a4bbcf087caa644edc07652fa8
Daniel Noyola <daniel.noyola.dev@gmail.com>
2020-02-10 07:44:40 -0600

    Updated pantheon script.

--- scripts/bash/pantheon-deploy.sh
+++ scripts/bash/pantheon-deploy.sh
@@ -1,35 +1,32 @@
 #!/usr/bin/env bash
 
-# Abort if anything fails
-set -e
-
 PROJECT_ROOT="${LANDO_MOUNT}"
 BUILD_DIR="$PROJECT_ROOT/build/pantheon"
 
 echo -e "Syncing repo..."
 if [ ! -d "$BUILD_DIR" ]; then
-  cd "${PROJECT_ROOT}/build"
+  cd "${PROJECT_ROOT}/build" || exit
   git clone "ssh://codeserver.dev.${PANTHEON_SITE_ID}@codeserver.dev.${PANTHEON_SITE_ID}.drush.in:2222/~/repository.git" pantheon
 else
-  cd "${BUILD_DIR}"
+  cd "${BUILD_DIR}" || exit
   git pull origin master
 fi
 
 # Copy Composer files.
-cd "${PROJECT_ROOT}"
+cd "${PROJECT_ROOT}" || exit
 cp composer.json "${BUILD_DIR}"
 cp composer.lock "${BUILD_DIR}"
 cp load.env.php "${BUILD_DIR}"
 
 # Remove build/html from composer path.
-cd "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit
 sed -i -e "s/build\/html\//web\//g" composer.json
 sed -i -e "s/build\/html\//web\//g" composer.lock
 sed -i -e "s/build\/drush/drush/g" composer.json
 sed -i -e "s/build\/drush/drush/g" composer.lock
 
 echo -e "Installing composer dependencies..."
-cd "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit
 composer install --no-dev
 
 # Copy custom files.
@@ -38,6 +35,14 @@ rsync -vzhr --delete "${PROJECT_ROOT}/src/config/" "${BUILD_DIR}/config"
 rsync -vzhr --delete "${PROJECT_ROOT}/src/profiles/" "${BUILD_DIR}/web/profiles/custom/"
 rsync -vzhr --delete "${PROJECT_ROOT}/src/modules/" "${BUILD_DIR}/web/modules/custom/"
 rsync -vzhr --delete --exclude '.gitignore' "${PROJECT_ROOT}/src/themes/" "${BUILD_DIR}/web/themes/custom"
+
+# get correct path fot splits.
+if [ -f "${BUILD_DIR}/web/libraries/config/default/config_split.config_split.dev.yml" ]; then
+  sed -i -e "s+../../src/+../+g" config/default/config_split.config_split.dev.yml
+fi
+if [ -f "${BUILD_DIR}/web/libraries/config/default/config_split.config_split.prod.yml" ]; then
+  sed -i -e "s+../../src/+../+g" config/default/config_split.config_split.prod.yml
+fi
 
 # Remove .git submodules
 echo -e "Removing .git submodules"
@@ -51,9 +56,9 @@ if [ -d "${BUILD_DIR}/web/themes/" ]; then
 fi
 cd "${BUILD_DIR}/vendor" && find . -name ".git" -exec rm -Rf {} \;
 
-cd "${PROJECT_ROOT}"
+cd "${PROJECT_ROOT}" || exit
 IT=$(git log -1 --pretty=format:"%an, %s - %ai"  $*)
-cd "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit
 echo -e "Commit and push"
 git add .
 echo "$IT"
